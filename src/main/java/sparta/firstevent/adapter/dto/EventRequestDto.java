65c9b670846b55d268bf3e7f41505886f71c537f
김대훈 <eogns136@gmail.com>
2026-01-22 11:26:15 +0900

    Feat: Dto 필드 추가, 생성자 선언

--- src/main/java/sparta/firstevent/adapter/dto/EventRequestDto.java
+++ src/main/java/sparta/firstevent/adapter/dto/EventRequestDto.java
@@ -2,9 +2,23 @@ package sparta.firstevent.adapter.dto;
 
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class EventRequestDto {
     private String title;
     private String description;
-    private int capacity;
+    private Integer capacity;
+    private LocalDateTime startAt;
+    private LocalDateTime endAt;
+
+    public EventRequestDto(
+            String title, String description, Integer capacity,
+            LocalDateTime startAt, LocalDateTime endAt) {
+        this.title = title;
+        this.description = description;
+        this.capacity = capacity;
+        this.startAt = startAt;
+        this.endAt = endAt;
+    }
 }
