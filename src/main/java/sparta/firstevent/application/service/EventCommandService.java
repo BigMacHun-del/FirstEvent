8f2067b2a4dade38198860c640848ddf83ee2b37
김대훈 <eogns136@gmail.com>
2026-01-22 11:32:10 +0900

    Feat: Service 로직 추가

--- src/main/java/sparta/firstevent/application/service/EventCommandService.java
+++ src/main/java/sparta/firstevent/application/service/EventCommandService.java
@@ -4,18 +4,27 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import sparta.firstevent.adapter.dto.EventRequestDto;
 import sparta.firstevent.application.parts.in.EventGetUseCase;
-import sparta.firstevent.application.parts.in.EventManageUserCase;
+import sparta.firstevent.application.parts.in.EventManageUseCase;
 import sparta.firstevent.application.parts.out.EventRepository;
 import sparta.firstevent.domain.event.Event;
 
 @Service
-@RequiredArgsConstructor
-public class EventCommandService implements EventManageUserCase {
+public class EventCommandService implements EventManageUseCase {
     private final EventRepository eventRepository;
 
+    public EventCommandService(EventRepository eventRepository) {
+        this.eventRepository = eventRepository;
+    }
 
     @Override
     public Event regist(EventRequestDto requestDto) {
         return eventRepository.save(Event.regist(requestDto));
+    }
+
+    @Override
+    public Event update(Long id, EventRequestDto eventRequestDto) {
+        Event event = eventRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("id에 해당하는 이벤트가 없습니다."));
+        event.update(eventRequestDto);
+        return event;
     }
 }
--- src/main/java/sparta/firstevent/application/service/EventQueryService.java
+++ src/main/java/sparta/firstevent/application/service/EventQueryService.java
@@ -1,11 +1,21 @@
 package sparta.firstevent.application.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 import sparta.firstevent.adapter.dto.EventRequestDto;
 import sparta.firstevent.application.parts.in.EventGetUseCase;
 import sparta.firstevent.application.parts.out.EventRepository;
 import sparta.firstevent.domain.event.Event;
 
-public class EventQueryService implements EventGetUseCase {
-    private  EventRepository eventRepository;
+import java.util.List;
 
+@Service
+@RequiredArgsConstructor
+public class EventQueryService implements EventGetUseCase {
+    private final EventRepository eventRepository;
+
+    @Override
+    public List<Event> getAll() {
+        return eventRepository.findAll();
+    }
 }
