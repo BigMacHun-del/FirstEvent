d3bc07bd8829334ef6ce773d68ed8da2afef7954
김대훈 <eogns136@gmail.com>
2026-01-22 11:33:57 +0900

    Feat: out 메서드 추가

--- src/main/java/sparta/firstevent/application/parts/out/EventRepository.java
+++ src/main/java/sparta/firstevent/application/parts/out/EventRepository.java
@@ -3,5 +3,13 @@ package sparta.firstevent.application.parts.out;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sparta.firstevent.domain.event.Event;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Event save(Event event);
+
+    Optional<Event> findById(Long id);
+
+    List<Event> findAll();
 }
